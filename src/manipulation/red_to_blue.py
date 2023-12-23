47d167917026fc96438c2d42ee894bd978e483c1
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-12-23 10:34:48 -0600

    Added intensity to red and yellow

--- src/manipulation/red_to_blue.py
+++ src/manipulation/red_to_blue.py
@@ -5,6 +5,7 @@ def red_to_blue(image):
         for x in range(image.width):
             r,g,b,a = pixels[x,y]
             if r > 100 and g < 100 and b < 100:
-                pixels[x,y] = (0,0,255,a)
+                red_intensity = r // 2
+                pixels[x,y] = (0,0,red_intensity,a)
     image = image.convert('RGB')
     return image
--- src/manipulation/yellow_to_blue.py
+++ src/manipulation/yellow_to_blue.py
@@ -3,8 +3,9 @@ def yellow_to_blue(image):
     pixels = image.load()
     for y in range(image.height):
         for x in range(image.width):
-            r,g,b,a = pixels[x,y]
+            r, g, b, a = pixels[x, y]
             if r > 100 and g > 100 and b < 100:
-                pixels[x,y] = (0,0,255,a)
+                yellow_intensity = (r + g) // 2
+                pixels[x, y] = (0, 0, yellow_intensity, a)
     image = image.convert('RGB')
     return image
